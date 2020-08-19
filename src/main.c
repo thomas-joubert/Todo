5cc70a349608cbb2162d932592f69e542e5db8c2
thomas.joubert <thomas.joubert@epita.fr>
2020-08-19 17:57:03 +0200

    [main] : instantiated the head of the tasks linked list

--- src/main.c
+++ src/main.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "task.h"
+#include "linked_tasks.h"
+
 int main(int argc, char *argv[])
 {
+    struct tasks_list *head = calloc(sizeof(struct tasks_list), 1);
+    if (!head)
+    {
+        fprintf(stderr, "Calloc failed\n");
+        exit(4);
+    }
+    
     if (argc == 1)
     {
-        //show_list()
+        //list_tasks(head)
         puts("fine");
     }
     else
