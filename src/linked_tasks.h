2127722d3458536867b00625ba47d1b9e923571b
thomas.joubert <thomas.joubert@epita.fr>
2020-08-19 17:50:04 +0200

    [linked_tasks] : linked list for task handling

---
+++ src/linked_tasks.h
@@ -0,0 +1,15 @@
+#ifndef LINKED_TASK_H
+#define LINKED_TASK_H
+
+#include "task.h"
+
+struct tasks_list
+{
+    struct task *next;
+    struct task *prev;
+};
+
+void push_back(struct tasks_list *head, char *name, char *description);
+void pop_back(struct tasks_list *head);
+
+#endif // end of linked_task.h
