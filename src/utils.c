8063ee01602b5841df1fab2b3ee0cfb9c167b6f5
thomas.joubert <thomas.joubert@epita.fr>
2020-09-24 17:57:46 +0200

    [utils] : created utils.c, added implementation of help function

---
+++ src/utils.c
@@ -0,0 +1,10 @@
+#include "utils.h"
+
+void help(void)
+{
+    puts("USAGE : ./todo <options>\n
+            no options : show the task list\n
+            -a / --add <task> : create a new task\n
+            -d / --delete <task or number> : delete a task\n
+            -h / --help : show this message");
+}
