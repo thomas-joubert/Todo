ed7251ff94b03c721df4fd80d7626e6c34262406
thomas.joubert <thomas.joubert@epita.fr>
2020-09-24 17:57:01 +0200

    [utils] : removed implem of help function, added declaration of option parser

--- src/utils.h
+++ src/utils.h
@@ -3,13 +3,7 @@
 
 #include <stdio.h>
 
-void help(void)
-{
-    puts("USAGE : ./todo <options>\n
-            no options : show the task list\n
-            -a / --add <task> : create a new task\n
-            -d / --delete <task or number> : delete a task\n
-            -h / --help : show this message");
-}
+void help(void);
+void option_parser(int argc, char *argv[]);
 
 #endif
