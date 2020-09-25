48d1b6af815fa87f76b8db1de4b5a0b7b655cc34
thomas.joubert <thomas.joubert@epita.fr>
2020-09-25 17:41:34 +0200

    [option-parsing] : added enum and struct to represent options

--- src/utils.h
+++ src/utils.h
@@ -6,4 +6,17 @@
 void help(void);
 void option_parser(int argc, char *argv[]);
 
+enum options
+{
+    HELP = 0,
+    ADD,
+    DELETE
+};
+
+struct option
+{
+    enum options type;
+    char *arg;
+};
+
 #endif
