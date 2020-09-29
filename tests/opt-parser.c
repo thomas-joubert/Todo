1e9d97fc6a2602a89cbf7dadd1e9ed133847b293
thomas.joubert <thomas.joubert@epita.fr>
2020-09-29 17:41:12 +0200

    [test][add] : added options_parser tests

--- tests/opt-parser.c
+++ tests/opt-parser.c
@@ -13,3 +13,63 @@ Test(option, false_option)
     cr_assert(opt->opt_type == HELP,
             "Expected opt->opt_type to be of type HELP\n");
 }
+
+Test(option, help)
+{
+    int argc = 2;
+    char *argv[2] = {"todo", "-h"};
+
+    struct single_option *opt = option_parser(argc, argv);
+
+    cr_assert(opt->arg == NULL, "Expected opt->arg to be NULL, opt->arg == %s\n", opt->arg);
+    cr_assert(opt->opt_type == HELP,
+            "Expected opt->opt_type to be of type HELP\n");
+}
+
+Test(option, add)
+{
+    int argc = 3;
+    char *argv[3] = {"todo", "-a", "Hello"};
+
+    struct single_option *opt = option_parser(argc, argv);
+
+    cr_assert(strcmp(opt->arg, "Hello") == 0, "Expected opt->arg to be \"Hello\", opt->arg == %s\n", opt->arg);
+    cr_assert(opt->opt_type == ADD,
+            "Expected opt->opt_type to be of type ADD\n");
+}
+
+Test(option, delete)
+{
+    int argc = 3;
+    char *argv[3] = {"todo", "-d", "Hello"};
+
+    struct single_option *opt = option_parser(argc, argv);
+
+    cr_assert(strcmp(opt->arg, "Hello") == 0, "Expected opt->arg to be \"Hello\", opt->arg == %s\n", opt->arg);
+    cr_assert(opt->opt_type == DELETE,
+            "Expected opt->opt_type to be of type DELETE\n");
+}
+
+Test(option, add_no_arg)
+{
+    int argc = 2;
+    char *argv[2] = {"todo", "-a"};
+
+    struct single_option *opt = option_parser(argc, argv);
+
+    cr_assert(opt->arg == NULL, "Expected opt->arg to be NULL, opt->arg == %s\n", opt->arg);
+    cr_assert(opt->opt_type == HELP,
+            "Expected opt->opt_type to be of type HELP\n");
+}
+
+Test(option, delete_no_arg)
+{
+    int argc = 2;
+    char *argv[2] = {"todo", "-d"};
+
+    struct single_option *opt = option_parser(argc, argv);
+
+    cr_assert(opt->arg == NULL, "Expected opt->arg to be NULL, opt->arg == %s\n", opt->arg);
+    cr_assert(opt->opt_type == HELP,
+            "Expected opt->opt_type to be of type HELP\n");
+}
