59b5fe320485a1cb8dcbd1ec042b2140efb24162
thomas.joubert <thomas.joubert@epita.fr>
2020-10-01 16:33:38 +0200

    [test][opt] : added a test on help

--- tests/opt-parser.c
+++ tests/opt-parser.c
@@ -73,3 +73,15 @@ Test(option, delete_no_arg)
     cr_assert(opt->opt_type == HELP,
             "Expected opt->opt_type to be of type HELP\n");
 }
+
+Test(option, help_with_opt)
+{
+    int argc = 3;
+    char *argv[3] = {"todo", "-h", "Hello"};
+
+    struct single_option *opt = option_parser(argc, argv);
+
+    cr_assert(opt->arg == NULL, "Expected opt->arg to be NULL, opt->arg == %s\n", opt->arg);
+    cr_assert(opt->opt_type == HELP,
+            "Expected opt->opt_type to be of type HELP\n");
+}
