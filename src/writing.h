c18b36f6acc7022330145635acb3575259a5e5bb
thomas.joubert <thomas.joubert@epita.fr>
2020-10-02 21:43:24 +0200

    [design][csv_writer] : modified prototype to specify the file

--- src/writing.h
+++ src/writing.h
@@ -1,9 +1,11 @@
 #ifndef DEF_WRITING
 #define DEF_WRITING
 
+#include <stdio.h>
+
 #include "utils.h"
 
 // Take a task an option (add), and write the corresponding task into a csv file
-int csv_writer(struct single_option *opt);
+int csv_writer(struct single_option *opt, FILE *file);
 
 #endif
