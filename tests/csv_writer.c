8f23597ffec7480c680755d98b081af47bc92f9a
thomas.joubert <thomas.joubert@epita.fr>
2020-10-02 21:40:50 +0200

    [tests][csv] : created first test layout / utility function for csv writer

---
+++ tests/csv_writer.c
@@ -0,0 +1,35 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <err.h>
+
+#include "../src/writing.h"
+#include "../src/utils.h"
+
+//Since the size of a char = 1 byte, a file's size amounts to its number of char
+int file_size(FILE *file)
+{
+    fseek(file, 0, SEEK_END);
+    return ftell(file);
+}
+
+Test(csv_writer, empty_arg)
+{
+    FILE *file = fopen("tests.csv");
+
+    if (file == NULL)
+        err("Error during file opening\n");
+
+    struct single_option *opt =
+    {
+        ADD, NULL
+    };
+
+    int file_state_b = file_size(file);
+    int ret = csv_writer(opt);
+    int file_state_a = file_size(file);
+
+    cr_assert_eq(ret, -1, "Bad return value, should have returned -1, \
+            returned %d", ret);
+    cr_assert_eq(file_state_b, file_state_a, "The file has been modified by %d\
+            characters", file_state_a - file_state_b);
+}
