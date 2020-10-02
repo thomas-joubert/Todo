703dd139c36147c54fe1ab10fa018be316cd4f5b
thomas.joubert <thomas.joubert@epita.fr>
2020-10-02 21:00:28 +0200

    [refacto][task] : removed struct task due to redundancy with single_option struct

--- src/task/task.h
+++ src/task/task.h
@@ -4,17 +4,9 @@
 #include <time.h>
 
 #include "../linked_tasks.h"
+#include "../utils.h"
 
 struct tasks_list;
-
-struct task
-{
-    int id;
-    // Max 70 character
-    char *name;
-    // Max 500 character
-    char *description;
-};
 
 void list_tasks(struct tasks_list *head);
 void print_tasks(struct tasks_list *head);
